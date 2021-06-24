1202d3a074cb40a2c00709307d2e1f65a43f62d1
test-aws <adyuri1257@gmail.com>
2021-06-24 20:58:55 +0900

    test

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,11 @@
-echo start APPLICATION_START
+#!/bin/bash
 
+#give permission for everything in the express-app directory
 sudo chmod -R 777 /home/ec2-user/test-service
 
-nohup node dist/bundle.js
+echo hello1
+ls
 
+#start our node app in the background
+nohup node dist/bundle.js
+echo finished
