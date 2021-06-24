40e02ebbe46544bb4e6e96c05644f50b6bdfb130
test-aws <adyuri1257@gmail.com>
2021-06-24 16:57:55 +0900

    testt

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-cd /home/ec2-user/test-service
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
-yum -y install nodejs npm
+
+#download node and npm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+
+#create our working directory if it doesnt exist
+DIR="/home/ec2-user/test-service"
+if [ -d "$DIR" ]; then
+  echo "${DIR} exists"
+else
+  echo "Creating ${DIR} directory"
+  mkdir ${DIR}
+fi
