88ce2db226efab0e4324ed2b8e2c6bf2c341d124
test-aws <adyuri1257@gmail.com>
2021-06-24 19:51:34 +0900

    test

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,12 +4,12 @@
 sudo chmod -R 777 /home/ec2-user/test-service
 
 #navigate into our working directory where we have all our github files
-cd /home/ec2-user/test-service
+# cd /home/ec2-user/test-service
 
 #add npm and node to path
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
 # npm install
@@ -17,5 +17,5 @@ echo hello1
 ls
 
 #start our node app in the background
-node dist/bundle.js
+nohup node dist/bundle.js
 echo finished
